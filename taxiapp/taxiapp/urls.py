0cb7dc8dbb78a689da8c1f7f8f59e85eaa59dd00
devmcdonald <dpm360@nyu.edu>
2024-09-01 19:05:02 -0400

    modified:   taxiapp/taxiapp/settings.py 	modified:   taxiapp/taxiapp/urls.py

--- taxiapp/taxiapp/urls.py
+++ taxiapp/taxiapp/urls.py
@@ -38,6 +38,3 @@ urlpatterns = [
     path("user/", include("user.urls")),
 ]
 
-# add at the last
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
